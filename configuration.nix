f5f25c07b512c266a2ecbd966a84d826a2b2d935
hoop3r <nhoop2107@gmail.com>
2024-12-16 13:21:41 -0500

    update steam config

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,13 @@
 
   programs.firefox.enable = true;
 
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true;
+  dedicatedServer.openFirewall = true; 
+  localNetworkGameTransfers.openFirewall = true; 
+  };
+  
   environment.systemPackages = with pkgs; [
     home-manager
     git    
--- modules/programs.nix
+++ modules/programs.nix
@@ -14,11 +14,4 @@
     steam
   ];
 
-  programs.steam = {
-  enable = true;
-  remotePlay.openFirewall = true;
-  dedicatedServer.openFirewall = true; 
-  localNetworkGameTransfers.openFirewall = true; 
-  };
-
 }
