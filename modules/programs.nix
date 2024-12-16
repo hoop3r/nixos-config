f26c4f1376048ac404f34ee35a62ab5a6c9d52fa
hoop3r <nhoop2107@gmail.com>
2024-12-16 13:18:48 -0500

    update modules

--- modules/programs.nix
+++ modules/programs.nix
@@ -13,4 +13,12 @@
     discord
     steam
   ];
+
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true;
+  dedicatedServer.openFirewall = true; 
+  localNetworkGameTransfers.openFirewall = true; 
+  };
+
 }
