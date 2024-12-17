81e26ea32ab4b48823d2e6fe02297e13a73da484
hoop3r <nhoop2107@gmail.com>
2024-12-16 19:22:38 -0500

    update inputs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
-    }
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
