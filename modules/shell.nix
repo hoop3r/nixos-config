bf5cede34a95309d34b04933a4321d1c1acfa605
hoop3r <nhoop2107@gmail.com>
2025-01-24 11:06:55 -0500

    update dotnet shell

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,28 +1,26 @@
-{ pkgs ? import <nixpkgs> {} }:
+with import <nixpkgs> {};
+pkgs.mkShell rec {
 
-pkgs.mkShell {
+  dotnetPkg = 
+    (with dotnetCorePackages; combinePackages [
+      sdk_7_0
+    ]);
 
-    dotnetPkg = 
-        (with dotnetCorePackages; combinePackages [
-        sdk_7_0
-        ]);
-        
-    deps = [
-        zlib
-        zlib.dev
-        openssl
-        dotnetPkg
-    ];
+  deps = [
+    zlib
+    zlib.dev
+    openssl
+    dotnetPkg
+  ];
 
-    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath ([
-        stdenv.cc.cc
-    ] ++ deps);
-    NIX_LD = "${pkgs.stdenv.cc.libc_bin}/bin/ld.so";
-    nativeBuildInputs = [ 
-    ] ++ deps;
+  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath ([
+    stdenv.cc.cc
+  ] ++ deps);
+  NIX_LD = "${pkgs.stdenv.cc.libc_bin}/bin/ld.so";
+  nativeBuildInputs = [ 
+  ] ++ deps;
 
-    shellHook = ''
-        DOTNET_ROOT="${dotnetPkg}";
-    '';
-
+  shellHook = ''
+    DOTNET_ROOT="${dotnetPkg}";
+  '';
 }
