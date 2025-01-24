40e4300f0353e2e2a130432c4d2e39049bdb199e
hoop3r <nhoop2107@gmail.com>
2025-01-24 12:05:47 -0500

    dotnet v8

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 
 let
-  dotnet_sdk = pkgs.dotnetCorePackages.dotnet_9.sdk;
-  dotnet_runtime = pkgs.dotnetCorePackages.dotnet_9.runtime;
-  dotnet_aspnetcore = pkgs.dotnetCorePackages.dotnet_9.aspnetcore;
+  dotnet_sdk = pkgs.dotnetCorePackages.dotnet_8.sdk;
+  dotnet_runtime = pkgs.dotnetCorePackages.dotnet_8.runtime;
+  dotnet_aspnetcore = pkgs.dotnetCorePackages.dotnet_8.aspnetcore;
 
   dotnet-full = with pkgs.dotnetCorePackages; combinePackages [
     dotnet_sdk
