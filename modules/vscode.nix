e96278c569949dc7041c1d04c7c8b5594a518051
hoop3r <nhoop2107@gmail.com>
2025-01-24 11:38:38 -0500

    test vscode config

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,42 +1,41 @@
 { pkgs, ... }:
-let
-  dotnet-full =
-    with pkgs.dotnetCorePackages;
-    combinePackages [
-      sdk_8_0_3xx
-      dotnet_8.runtime
-      dotnet_8.aspnetcore
-    ];
 
-  deps = (
-    ps:
-    with ps;
-    [
-      zlib
-      openssl.dev
-      pkg-config
-      stdenv.cc
-      cmake
-      mono
-      msbuild
-    ]
-    ++ [ dotnet-full ]
-  );
+let
+  dotnet_sdk = pkgs.dotnetCorePackages.sdk_8_0_3xx;
+  dotnet_runtime = pkgs.dotnetCorePackages.dotnet_8.runtime;
+  dotnet_aspnetcore = pkgs.dotnetCorePackages.dotnet_8.aspnetcore;
+
+  dotnet_full = with pkgs.dotnetCorePackages; combinePackages [
+    dotnet_sdk
+    dotnet_runtime
+    dotnet_aspnetcore
+  ];
+
+  dotnet_deps = [
+    mono
+    msbuild
+  ];
+
+  dev_deps = [
+    zlib
+    openssl.dev
+    pkg-config
+    stdenv.cc
+    cmake
+  ];
+
+  deps = ps: with ps; dev_deps ++ dotnet_deps ++ [ dotnet_full ];
 in
 {
   programs.vscode = {
     enable = true;
-    package =
-      (pkgs.vscode.overrideAttrs (prevAttrs: {
-        nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
-        postFixup =
-          prevAttrs.postFixup
-          + ''
-            wrapProgram $out/bin/code \
-              --set DOTNET_ROOT "${dotnet-full}" \
-              --prefix PATH : "~/.dotnet/tools"
-          '';
-      })).fhsWithPackages
-        (ps: deps ps);
+    package = pkgs.vscode.overrideAttrs (prevAttrs: {
+      nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+      postFixup = prevAttrs.postFixup + ''
+        wrapProgram $out/bin/code \
+          --set DOTNET_ROOT "${dotnet_full}" \
+          --prefix PATH : "~/.dotnet/tools"
+      '';
+    }).fhsWithPackages (ps: deps ps);
   };
 }
