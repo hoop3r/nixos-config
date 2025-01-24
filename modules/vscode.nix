57a074c2b521750386ca2078841b718c93ea90ac
hoop3r <nhoop2107@gmail.com>
2025-01-24 12:00:04 -0500

    update dotnet vscode

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 
 let
-  dotnet_sdk = pkgs.dotnetCorePackages.sdk_8_0_3xx;
-  dotnet_runtime = pkgs.dotnetCorePackages.dotnet_8.runtime;
-  dotnet_aspnetcore = pkgs.dotnetCorePackages.dotnet_8.aspnetcore;
+  dotnet_sdk = pkgs.dotnetCorePackages.dotnet_9.sdk;
+  dotnet_runtime = pkgs.dotnetCorePackages.dotnet_9.runtime;
+  dotnet_aspnetcore = pkgs.dotnetCorePackages.dotnet_9.aspnetcore;
 
-  dotnet_full = with pkgs.dotnetCorePackages; combinePackages [
+  dotnet-full = with pkgs.dotnetCorePackages; combinePackages [
     dotnet_sdk
     dotnet_runtime
     dotnet_aspnetcore
@@ -18,20 +18,24 @@ let
     stdenv.cc
     cmake
     mono
-    msbuild
-  ] ++ [ dotnet_full ];
+    # msbuild
+  ] ++ [ dotnet-full ];
 in
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode.overrideAttrs (prevAttrs: {
-      nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
-      postFixup = prevAttrs.postFixup + ''
-        wrapProgram $out/bin/code \
-          --set DOTNET_ROOT "${dotnet_full}" \
-          --prefix PATH : "~/.dotnet/tools"
-      '';
-    }).fhsWithPackages (ps: deps ps);
-    extensions = [];
+    package =
+      (pkgs.vscode.overrideAttrs (prevAttrs: {
+        nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+        postFixup =
+          prevAttrs.postFixup
+          + ''
+            wrapProgram $out/bin/code \
+              --set DOTNET_ROOT "${dotnet-full}" \
+              --prefix PATH : "~/.dotnet/tools"
+          '';
+      })).fhsWithPackages
+        (ps: deps ps);
+    extensions = [  ];
   };
 }
