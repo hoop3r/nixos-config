7bdeed2360573236696e2dd9036f5a3726e706c6
hoop3r <nhoop2107@gmail.com>
2025-02-10 11:54:10 -0500

    update shell config

--- modules/programs.nix
+++ modules/programs.nix
@@ -34,6 +34,17 @@
         ];
       };
     };
+
+    zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      ohMyZsh = {
+        enable = true;
+        plugins = [ "git" "docker" ];
+      }
+    };
+
   };
 
   home.packages = with pkgs; [
