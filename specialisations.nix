7fe4631621a71534cb7b740bc50faaffd5a841e7
hoop3r <nhoop2107@gmail.com>
2025-11-14 14:00:49 -0500

    fix: update specialisation references

--- specialisations.nix
+++ specialisations.nix
@@ -3,36 +3,36 @@
   specialisation.hyprland = {
     inheritParentConfig = true;
     configuration = {
-      services.xserver.desktopManager.gnome.enable = false;
-      services.xserver.desktopManager.plasma6.enable = false;
+      services.desktopManager.gnome.enable = false;
+      services.desktopManager.plasma6.enable = false;
       programs.hyprland.enable = true;
+      programs.hyprland.withUWSM = true;
       programs.hyprland.xwayland.enable = true;
-      hardware.bluetooth.enable = true;
-      hardware.bluetooth.powerOnBoot = true;
-      services.xserver.displayManager.sddm.enable = false;
+      # hardware.bluetooth.enable = true;
+      # hardware.bluetooth.powerOnBoot = true;
     };
   };
   specialisation.gnome = {
     inheritParentConfig = true;
     configuration = {
-      services.xserver.desktopManager.gnome.enable = true;
+      services.xserver.displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
       programs.hyprland.enable = false;
-      services.xserver.displayManager.sddm.enable = false;
-      services.xserver.desktopManager.plasma6.enable = false;
+    
+      services.desktopManager.plasma6.enable = false;
     };
 
   };
-  # specialisation.kde = {
-  #   inheritParentConfig = true;
-  #   configuration = {
-  #     services.xserver.displayManager.gdm.enable = pkgs.lib.mkForce false;
-  #     services.xserver.displayManager.sddm.enable = pkgs.lib.mkForce true;
-
-  #     programs.hyprland.enable = false;
-  #     services.xserver.desktopManager.gnome.enable = false;
-  #     services.xserver.desktopManager.plasma6.enable = true;
-  #     services.xserver.displayManager.defaultSession = "plasma";
-  #   };
-  # };
+  specialisation.kde = {
+    inheritParentConfig = true;
+    configuration = {
+      services.xserver.displayManager.gdm.enable = pkgs.lib.mkForce false;
+      services.xserver.displayManager.sddm.enable = pkgs.lib.mkForce true;
+      programs.hyprland.enable = false;
+      services.xserver.desktopManager.gnome.enable = false;
+      services.xserver.desktopManager.plasma6.enable = true;
+      services.xserver.displayManager.defaultSession = "plasma";
+    };
+  };
 }
 
